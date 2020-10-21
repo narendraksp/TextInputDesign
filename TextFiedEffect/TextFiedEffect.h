5d1b055fb85f71d02d03781488121c0465e1143f
narendra <narendra.ksp@icloud.com>
2020-10-21 10:40:02 +0530

    border textfied

---
+++ TextFiedEffect/TextFiedEffect.h
@@ -0,0 +1,19 @@
+//
+//  TextFiedEffect.h
+//  TextFiedEffect
+//
+//  Created by narendra kumar s on 21/10/20.
+//  Copyright © 2020 narendra kumar s. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TextFiedEffect.
+FOUNDATION_EXPORT double TextFiedEffectVersionNumber;
+
+//! Project version string for TextFiedEffect.
+FOUNDATION_EXPORT const unsigned char TextFiedEffectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TextFiedEffect/PublicHeader.h>
+
+
